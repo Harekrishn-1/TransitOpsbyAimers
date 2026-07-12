ca9fb50e14d0213029d7de8b44db46ff2a32daf1
Akash Sarkar <akashsarkarakash123@gmail.com>
2026-07-12 09:25:12 +0530

    this commit is after the project initiate

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "TransitOps Backend Running 🚀"
+    });
+});
+
+module.exports = app;
